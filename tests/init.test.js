1d317b3af157797cfb1106968231e880212a01a3
eirinimix <michaltsii@gmail.com>
2022-12-07 18:45:29 +0200

    [#test] Added first test.
    [wip]

--- tests/init.test.js
+++ tests/init.test.js
@@ -5,14 +5,20 @@ const http = require('node:http');
 const test = require('ava').default;
 const got = require('got');
 const listen = require('test-listen');
+const Source = require('../src/models/source');
 
 const app = require('../src/index');
-const {jwtSign} = require('../src/utilities/authentication/helpers');
+const { jwtSign } = require('../src/utilities/authentication/helpers');
 
 test.before(async (t) => {
   t.context.server = http.createServer(app);
   t.context.prefixUrl = await listen(t.context.server);
-  t.context.got = got.extend({http2: true, throwHttpErrors: false, responseType: 'json', prefixUrl: t.context.prefixUrl});
+  t.context.got = got.extend({
+    http2: true,
+    throwHttpErrors: false,
+    responseType: 'json',
+    prefixUrl: t.context.prefixUrl,
+  });
 });
 
 test.after.always((t) => {
@@ -20,14 +26,28 @@ test.after.always((t) => {
 });
 
 test('GET /statistics returns correct response and status code', async (t) => {
-  const {body, statusCode} = await t.context.got('general/statistics');
+  const { body, statusCode } = await t.context.got('general/statistics');
   //t.is(body.sources, 1);
   t.assert(body.success);
   t.is(statusCode, 200);
 });
 
 test('GET /sources returns correct response and status code', async (t) => {
-  const token = jwtSign({id: 1});
-  const {statusCode} = await t.context.got(`sources/sources?token=${token}`);
+  const token = jwtSign({ id: 1 });
+  const { statusCode } = await t.context.got(`sources/sources?token=${token}`);
   t.is(statusCode, 200);
+});
+test('POST /sources returns correct response and status code', async (t) => {
+  const token = jwtSign({ id: 1 });
+
+  const api = await t.context.got.extend({
+    responseType: 'json',
+  });
+
+  const body = new Source({name: 'Name'});
+  const { statusCode } = await api(`sources/create-source?token=${token}`, {
+    method: 'POST',
+    json: body
+  });
+   t.is(statusCode, 200);
 });
