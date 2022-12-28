4ec77b427b9d80624f99b8bac8ff415c875a3622
eirinimix <michaltsii@gmail.com>
2022-12-28 18:35:04 +0200

    Cleaned code.

--- tests/general.test.js
+++ tests/general.test.js
@@ -7,9 +7,6 @@ const got = require('got');
 const listen = require('test-listen');
 const Source = require('../src/models/source');
 const app = require('../src/index');
-const User = require('../src/models/user');
-const { jwtSign } = require('../src/utilities/authentication/helpers');
-let user;
 const sinon = require('sinon');
 
 test.before(async (t) => {
@@ -21,16 +18,10 @@ test.before(async (t) => {
     responseType: 'json',
     prefixUrl: t.context.prefixUrl,
   });
-  user = await User.create({
-    username: 'user',
-    password: 'password',
-    email: 'email',
-  });
 });
 
 test.after.always((t) => {
   t.context.server.close();
-  User.findByIdAndDelete(user._id);
 });
 test.beforeEach(() => {
   sinon.resetHistory();
@@ -40,42 +31,46 @@ test.beforeEach(() => {
 // GET STATISTICS
 test('GET /statistics returns correct response and status code', async (t) => {
   const { body, statusCode } = await t.context.got('general/statistics');
-    // Assert that the request is successful from body
+  // Assert that the request is successful from body
   t.assert(body.success);
-    // Assert that the status code of the response is 200
+  // Assert that the status code of the response is 200
   t.is(statusCode, 200);
 });
 // GET STATISTICS ERROR
 test('GET /statistics returns correct response and status code for error', async (t) => {
   const findStub = sinon
-  .stub(Source, 'countDocuments')
-  .throws(new Error('Something went wrong'));
+    .stub(Source, 'countDocuments')
+    .throws(new Error('Something went wrong'));
   // Send a GET request to the /sources route with the token as a query parameter
   const { body, statusCode } = await t.context.got('general/statistics');
   // Assert that the status code of the response is 404
   t.is(statusCode, 404);
-  ;
   findStub.restore();
 });
 
 // GET TEST-URL
 test('GET /test-url returns correct status code', async (t) => {
-  const { body, statusCode } = await t.context.got('general/test-url');
-    // Assert that the status code of the response is 200
+  const { body, statusCode } = await t.context.got(
+    'general/test-url?url=https://www.google.gr'
+  );
+  // Assert that the status code of the response is 200
   t.is(statusCode, 200);
 });
-
 // GET TEST-URL ERROR
-test('GET /test-url returns correct status code for error', async (t) => {
-  const findStub = sinon
-  .stub(Source, 'countDocuments')
-  .throws(new Error('Something went wrong'));
-  // Send a GET request to the /sources route with the token as a query parameter
-  const { body, statusCode } = await t.context.got('general/test-url');
-  // Assert that the status code of the response is 404
-  t.is(statusCode, 404);
-  ;
-  findStub.restore();
+test('GET /test-url Returns 500 status and active: false on error', async (t, done) => {
+  // Create a stub for the got.get method
+  const gotStub = sinon
+    .stub(got, 'get')
+    .throws(new Error('Something went wrong'));
+  // Make the got.get stub reject with an error
+  const { body, statusCode } = await t.context.got(
+    'general/test-url?url=http://thisurldoesnotexist'
+  );
+
+  t.is(body.status, 500);
+  t.is(body.active, false);
+  sinon.restore();
+});
 // GET TEST-URL-REQUEST GET
 test('GET /test-url-request Sends GET request and returns response', async (t) => {
   // Send an HTTP GET request to the /test-url-request route
