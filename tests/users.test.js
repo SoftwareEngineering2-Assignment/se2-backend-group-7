5e50cae30cc5a56ee72ad66ca5a09b25a2dec20c
Maria Xouri <mariaxouri11@gmail.com>
2023-01-05 14:06:08 +0200

    Users test

--- tests/users.test.js
+++ tests/users.test.js
@@ -1,23 +1,22 @@
-/* eslint-disable import/no-unresolved */
+    /* eslint-disable import/no-unresolved */
 require('dotenv').config();
 
-
-//const {env} = require('../src/tests/.env');
+// const {env} = require('../src/tests/.env');
 const http = require('node:http');
 const test = require('ava').serial;
 const got = require('got');
 
 const listen = require('test-listen');
 
+const sinon = require('sinon');
 const Source = require('../src/models/source');
 const app = require('../src/index');
 const User = require('../src/models/user');
 const Reset = require('../src/models/reset');
 
-const { jwtSign } = require('../src/utilities/authentication/helpers');
+const {jwtSign} = require('../src/utilities/authentication/helpers');
 
 let user;
-const sinon =  require('sinon');
 
 test.before(async (t) => {
   t.context.server = http.createServer(app);
@@ -26,10 +25,10 @@ test.before(async (t) => {
     http2: true, 
     throwHttpErrors: false, 
     responseType: 'json',
-     prefixUrl: t.context.prefixUrl,
-    });
+    prefixUrl: t.context.prefixUrl,
+  });
 
-  user = await  User.create({
+  user = await User.create({
     username: 'user',
     password: 'password',
     email: 'bill@gmail.com ',
@@ -43,179 +42,161 @@ test.after.always((t) => {
 
 test.beforeEach(() => {
   sinon.resetHistory();
-    sinon.restore(); 
+  sinon.restore(); 
   sinon.reset(); 
 });
-               //CREATE 
+// CREATE 
 
- test('POST /users create user with valid data - 200 ',async  (t) => {
-   // const token = jwtSign({ id: 1});
-    const api = await t.context.got.extend({
-     responseType:'json',
-    });
-   // let newUser={...user};
-    //newUser.id= user._id;
-    const body = new  User({username:'new_user893', password:'new_password89', email:'kostas89@gmail.com'});
-    const { statusCode } = await api(`users/create`, {
-      method: 'POST',
-      json: body,
-    }); 
-    console.log(body);
-    t.is(statusCode,200);
+test('POST /users create user with valid data - 200 ', async (t) => {
+  // const token = jwtSign({ id: 1});
+  const api = await t.context.got.extend({responseType: 'json', });
+  // let newUser={...user};
+  // newUser.id= user._id;
+  const body = new User({username: 'new_user893', password: 'new_password89', email: 'kostas89@gmail.com'});
+  const {statusCode} = await api('users/create', {
+    method: 'POST',
+    json: body,
+  }); 
+  console.log(body);
+  t.is(statusCode, 200);
 
-    //t.is(token,newUser.id);
-  });
+  // t.is(token,newUser.id);
+});
                
-  test('POST /users create user that already exists - 409 ',async  (t) => {
-    
-    const api = await t.context.got.extend({
-     responseType:'json',
-    });
+test('POST /users create user that already exists - 409 ', async (t) => {
+  const api = await t.context.got.extend({responseType: 'json', });
 
-    const request = new User({username: 'user',password:'password',email:'bill@gmail.com '});
-    const {body} =await api(`users/create`,{
-      method: 'POST',
-      json: request,
-    });
-    t.is(body.status,409);
-    t.is(body.message,'Registration Error: A user with that e-mail or username already exists.');
+  const request = new User({username: 'user', password: 'password', email: 'bill@gmail.com '});
+  const {body} = await api('users/create', {
+    method: 'POST',
+    json: request,
   });
+  t.is(body.status, 409);
+  t.is(body.message, 'Registration Error: A user with that e-mail or username already exists.');
+});
   
-  
-  test('POST /users authendicate user, user not found - 401', async (t)=> {
-    const api = await t.context.got.extend({
-      responseType:'json',
-    });
-    const request = new User({username: 'usern',password: '56789'});
-    const {body} = await api(`users/authenticate`,{
-      method:'POST',
-      json: request,
-    });
-    t.is(body.status,401);
-    t.is(body.message, 'Authentication Error: User not found.');
+test('POST /users authendicate user, user not found - 401', async (t) => {
+  const api = await t.context.got.extend({responseType: 'json', });
+  const request = new User({username: 'usern', password: '56789'});
+  const {body} = await api('users/authenticate', {
+    method: 'POST',
+    json: request,
   });
-   test('POST /users authentication password doesnot match -401',async (t) =>{
-    const api = await t.context.got.extend({
-      responseType:'json',
-    });
+  t.is(body.status, 401);
+  t.is(body.message, 'Authentication Error: User not found.');
+});
+test('POST /users authentication password doesnot match -401', async (t) => {
+  const api = await t.context.got.extend({responseType: 'json', });
 
-    const request = new User({username: 'new_user', password: '5566788'});
-    const{body} = await api(`users/authenticate`,{
-      method: 'POST',
-      json: request,
-    });
-    //console.log(body);
-    t.is(body.status,401);
-   // t.is(body.message,'Authentication Error: Password does not match!');
-   })
+  const request = new User({username: 'new_user', password: '5566788'});
+  const {body} = await api('users/authenticate', {
+    method: 'POST',
+    json: request,
+  });
+    // console.log(body);
+  t.is(body.status, 401);
+  // t.is(body.message,'Authentication Error: Password does not match!');
+});
   
-   test('POST /users authentication password is correct -200',async (t) =>{
-   // const token = jwtSign({username:user.username, id: user._id, email:user.email });
-    const token = jwtSign({id:1 });
+test('POST /users authentication password is correct -200', async (t) => {
+  // const token = jwtSign({username:user.username, id: user._id, email:user.email });
+  const token = jwtSign({id: 1});
 
-    const api = await t.context.got.extend({
-      responseType:'json',
-    });
-    const body = new User({username:'new_user', password: 'new_password', email:'kostas@gmail.com'});
-    const{statusCode} = await api(`users/authenticate?token=${token}`,{
-      method:'POST',
-      json: body,
-    });
-    console.log(body);
-    t.is(statusCode, 200); 
-   });
-   test('POST /users reset password -404',async (t) => {
-    //const token = jwtSign({ id: user._id });
-    const api = await t.context.got.extend({
-      responseType:'json',
-    });
+  const api = await t.context.got.extend({responseType: 'json', });
+  const body = new User({username: 'new_user', password: 'new_password', email: 'kostas@gmail.com'});
+  const {statusCode} = await api(`users/authenticate?token=${token}`, {
+    method: 'POST',
+    json: body,
+  });
+  console.log(body);
+  t.is(statusCode, 200); 
+}); 
 
-    const request = new User({username:'new_user' , password:'89887' , email:'kostasPap@gmail.com'});
-    const {body} = await api(`users/resetpassword`,{
-      method: 'POST',
-      json: request,
-    });
+test('POST /users reset password correct', async (t) => {
+  const api = await t.context.got.extend({responseType: 'json',});
 
-   // console.log(body);
-    t.is(body.status,404);
-    t.is(body.message, 'Resource Error : User not found.');
-      
-   }); 
+  const request = {username: 'user'};
+  const {body} = await api('users/resetpassword', {
+    method: 'POST',
+    json: request,
+  });
 
-   test('POST /users change password -404',async (t) =>{
-    const token = jwtSign({ id: user._id });
-    const api = await t.context.got.extend ({
-      responseType: 'json',
-    });
-    
-    const request = new User({username:'new_user',password: '12345678',email: 'kostas@gmail.com'});
-    const {body} = await api(`users/changepassword?token=${token}`,{
-      method: 'POST',
-      json: request,
-    });
-    //console.log(body);
-    t.is(body.status,404);
-    t.is(body.message, 'Resource Error: User not found.');
-   });
+  t.is(body.ok, true);
+  t.is(body.message, 'Forgot password e-mail sent.');
+}); 
 
-   test('POST /user reset token -404',async(t)=>{
-    const token = jwtSign({ id: 1});
-      const api = await t.context.got.extend({
-        responseType:'json',
-      });
-      const request = new User({username:'new_user5',password: '123456785',email: 'kostas5@gmail.com'});
-      const {body} = await api(`users/changepassword?token=${token}`,{
-        method: 'POST',
-        json: request,
-      });
-      console.log(body);
-      t.is(body.status,404);
-     // t.is(body.message, ' Resource Error: Reset token has expired.')
-    });
-   /*  test('POST /user reset token -410',async(t)=>{
-      const token = jwtSign({id:id._id});
-      const d = new Date("2016-03-25");
-     
-        const api = await t.context.got.extend({
-          responseType:'json',
-        });
-        
-        const request = new Reset({username:'new_user',token: token ,expireAt:d});
-        const {body,statusCode} = await api(`users/changepassword?token=${token}`,{
-          method:'GET',
-          json:request,
-        });
-      
-        console.log(body);
-        t.is(statusCode,410);
-       // t.is(body.message, ' Resource Error: Reset token has expired.')
+test('POST /users reset password user does not exist', async (t) => {
+  const api = await t.context.got.extend({responseType: 'json',});
+  // Giving the name of a user that does'nt exist
+  const request = {username: 'user_not_exist'};
+  const {body} = await api('users/resetpassword', {
+    method: 'POST',
+    json: request,
+  });
 
-   })*/
+  t.is(body.status, 404);
+  t.is(body.message, 'Resource Error: User not found.');
+});
 
-   test('POST /users changepssword  is correct -200',async (t) =>{
-    // const token = jwtSign({username:user.username, id: user._id, email:user.email });
-     const token = jwtSign({id:user._id });
+/* DOES NOT WORK PROPERLY
+test('POST /users changepassword  and return -410', async (t) => {
+  // Creating a new user το drop reset schema
+  const testUser = await User.create({
+    username: 'user_test',
+    password: 'password_test',
+    email: 'email',
+  });
+  const token = jwtSign({username: testUser.username});
  
-     const api = await t.context.got.extend({
-       responseType:'json',
-     });
-     const body = new User({username:'new_user58', password: '12345678', email:'kostas58@gmail.com'});
-     const{statusCode} = await api(`users/changepassword?token=${token}`,{
-       method:'POST',
-       json: body,
-     });
-     
-     console.log(body);
-     t.is(statusCode, 200);
-    // t.is(body.message, 'Password was changed.'); 
-    });
+  const api = await t.context.got.extend({responseType: 'json'});
+  const request = {password: 'new_password'};
+  // Drop reset in order to be null and return 410
+  //await Reset.findOneAndRemove({username: testUser.username});
+  const {body} = await api(`users/changepassword?token=${token}`, {
+    method: 'POST',
+    json: request,
+  });
+  console.log(body);
+  t.is(body.status, 410);
+  t.is(body.message, 'Resource Error: Reset token has expired.'); 
+});    */
+
+test('POST /users changepassword  changes password correctly', async (t) => {
+  const token = jwtSign({username: user.username});
+ 
+  const api = await t.context.got.extend({responseType: 'json'});
+  const request = {password: 'new_password'};
+  
+  const {body, statusCode} = await api(`users/changepassword?token=${token}`, {
+    method: 'POST',
+    json: request,
+  });
+ 
+  t.is(statusCode, 200);
+  t.is(body.ok, true);
+  t.is(body.message, 'Password was changed.'); 
+});   
+
+test('POST /users changepassword  user does not exist', async (t) => {
+  // Creating a token for a user that doesn't exist
+  const token = jwtSign({username: 'just_a_username'});
+ 
+  const api = await t.context.got.extend({responseType: 'json'});
+  const request = {password: 'new_password'};
+  // Making the call
+  const {body} = await api(`users/changepassword?token=${token}`, {
+    method: 'POST',
+    json: request,
+  });
+ 
+  t.is(body.status, 404);
+  t.is(body.message, 'Resource Error: User not found.'); 
+});   
+//  const body  = new User({ username:'usernameA'});
+// const { statusCode } = await api (`users/create-users?token=${token}`,{
+// method: 'POST',
+// json: body,
+// });
+// t.is(statusCode, 409);
 
 
-
-
-  //  const body  = new User({ username:'usernameA'});
-   // const { statusCode } = await api (`users/create-users?token=${token}`,{
-     // method: 'POST',
-      //json: body,
-    //});
-    //t.is(statusCode, 409);
